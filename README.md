8bff0f5b7138ca2edfef704f12e12473ce83a581
Rika <tjj.rikap@gmail.com>
2024-05-04 09:30:59 +0800

    v0.1.0-alpha

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # create-tailwind-vue
+
 Create a vite vue-ts project with optional extra packages
 
 [![npm](https://npmbadge.com/npm/create-tailwind-vue)](https://www.npmjs.com/package/create-tailwind-vue)
 
-* [tailwind](tailwindcss.com/)
-* [vue-router@4](https://router.vuejs.org/)
-* [pinia](pinia.vuejs.org/)
+- [tailwind](tailwindcss.com/)
+- [vue-router@4](https://router.vuejs.org/)
+- [pinia](pinia.vuejs.org/)
+- [vueuse](https://vueuse.org/)
+- [daisyui](https://daisyui.com/)
+- [font-awesome](https://fontawesome.com/)
--- index.js
+++ index.js
@@ -1,141 +1,117 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process');
-const fs = require('fs');
+const { execSync, exec } = require("child_process");
+const fs = require("fs");
 
 function isUsingYarn() {
-  return (process.env.npm_config_user_agent || '').indexOf('yarn') === 0;
+  return (process.env.npm_config_user_agent || "").indexOf("yarn") === 0;
 }
-console.log(`Using ${isUsingYarn() ? 'yarn' : 'npm'}...`);
+console.log(`Using ${isUsingYarn() ? "yarn" : "npm"}...`);
 
+class MainTsConfigure {
+  constructor({ projectName, packagesToInstall }) {
+    this.mainTsFilePath = "./src/main.ts";
+    this.packagesToInstall = packagesToInstall;
+    this.mainTsContent = {
+      imports: ["import { createApp } from 'vue'", "import App from './App.vue'", 'import "./style.css"'],
+      createApp: ["const app = createApp(App);"],
+      uses: [],
+      mount: ['app.mount("#app");'],
+    };
+    this.cmds = {
+      create() {
+        console.log(`Creating a new ${projectName} project...`);
+        execSync(isUsingYarn() ? `yarn create vite ${projectName} --template vue-ts` : `npx create-vite ${projectName} --template vue-ts`);
+        console.log(`${projectName} project created successfully.`);
+      },
+      chdir() {
+        process.chdir(projectName);
+      },
+      tailwindcss() {
+        console.log("Installing tailwindcss...");
+        execSync(isUsingYarn() ? "yarn add -D tailwindcss@latest postcss@latest autoprefixer@latest" : "npm install -D tailwindcss@latest postcss@latest autoprefixer@latest");
+        console.log("Configuring tailwindcss...");
+        execSync("npx tailwindcss init -p");
+        let tailwindcssJsContent = fs.readFileSync("tailwind.config.js", "utf-8");
+        tailwindcssJsContent = tailwindcssJsContent.replace(/'content\s*:\[\],/g, "'content': ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],");
+        fs.writeFileSync("tailwind.config.js", tailwindcssJsContent);
+        fs.writeFileSync("style.css", "@tailwind base;\n@tailwind components;\n@tailwind utilities;");
+        console.log("tailwindcss installed and configured successfully.");
+      },
+      "vue-router"() {
+        console.log("Installing vue-router...");
+        execSync(isUsingYarn() ? "yarn add vue-router@next" : "npm install vue-router@next --save");
+        console.log("Configuring vue-router...");
+        fs.writeFileSync(
+          "src/router.ts",
+          `import { createRouter, createWebHistory } from "vue-router";
 
-function createProject(projectName) {
-  console.log(`Creating a new ${projectName} project...`);
-  if (isUsingYarn())
-    execSync(`yarn create vite ${projectName} --template vue-ts`, { stdio: 'inherit' });
-  else
-    execSync(`npx create-vite ${projectName} --template vue-ts`, { stdio: 'inherit' });
-  console.log(`${projectName} project created successfully.`);
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("./views/Home.vue"),
+  },
+];
 
-  process.chdir(projectName);
-}
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
-function installPackage(packageName) {
-  console.log(`Installing ${packageName}...`);
-  if (isUsingYarn())
-  execSync(`yarn add ${packageName}`, { stdio: 'inherit' });
-  else  
-  execSync(`npm install ${packageName}`, { stdio: 'inherit' });
-  console.log(`${packageName} installed successfully.`);
-}
-
-function configureTailwind() {
-  console.log('Configuring Tailwind CSS...');
-
-  // Generate Tailwind configuration file
-  if (isUsingYarn())
-  execSync('yarn add -D postcss autoprefixer && yarn run tailwindcss init -p', { stdio: 'inherit' });
-  else
-  execSync('npm install -D postcss autoprefixer && npx tailwindcss init -p', { stdio: 'inherit' });
-
-  // Modify tailwind.config.js to include custom content
-  const tailwindConfigPath = './tailwind.config.js';
-  let tailwindConfigContent = fs.readFileSync(tailwindConfigPath, 'utf-8');
-
-  const contentRegex = /(content:\s*)\[.*?\]/s;
-  const contentReplacement = "content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx', './src/**/*.tsx']";
-  tailwindConfigContent = tailwindConfigContent.replace(contentRegex, contentReplacement);
-
-  fs.writeFileSync(tailwindConfigPath, tailwindConfigContent);
-
-  // Modify main.css to include Tailwind CSS
-  const cssFilePath = './src/style.css';
-  const cssContent = `@import 'tailwindcss/base';
-@import 'tailwindcss/components';
-@import 'tailwindcss/utilities';
-`;
-  fs.writeFileSync(cssFilePath, cssContent);
-
-  console.log('Tailwind CSS configured successfully.');
-}
-
-function configureVueRouter() {
-  console.log('Configuring Vue Router...');
-
-  // Modify main.ts to import and use Vue Router
-  const mainTsFilePath = './src/main.ts';
-  let mainTsContent = fs.readFileSync(mainTsFilePath, 'utf-8');
-
-  mainTsContent = mainTsContent.replace(
-    `import { createApp } from 'vue'`,
-    `import { createApp } from 'vue'\nimport { createRouter, createWebHistory } from 'vue-router'`
-  );
-
-  mainTsContent = mainTsContent.replace(
-    `createApp(App)`,
-    `const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-    { path: '/', component: Home },
-    // Add your routes here
-    ],
-})
-
-createApp(App).use(router)`
-  );
-
-  fs.writeFileSync(mainTsFilePath, mainTsContent);
-
-  console.log('Vue Router configured successfully.');
-
-  // Modify App.vue to include <router-view/> element
-  const appVueFilePath = './src/App.vue';
-  let appVueContent = fs.readFileSync(appVueFilePath, 'utf-8');
-
-  const templateRegex = /(<template>)/;
-  const templateReplacement = '$1\n  <router-view/>';
-  appVueContent = appVueContent.replace(templateRegex, templateReplacement);
-
-  fs.writeFileSync(appVueFilePath, appVueContent);
-
-  console.log('App.vue modified successfully.');
-}
-
-function configurePinia() {
-  console.log('Configuring Pinia...');
-
-  // Modify main.ts to import and use Pinia
-  const mainTsFilePath = './src/main.ts';
-  let mainTsContent = fs.readFileSync(mainTsFilePath, 'utf-8');
-
-  mainTsContent = mainTsContent.replace(
-    `import { createApp } from 'vue'`,
-    `import { createApp } from 'vue'\nimport { createPinia } from 'pinia'`
-  );
-
-  mainTsContent = mainTsContent.replace(
-    `createApp(App)`,
-    `createApp(App).use(createPinia())`
-  );
-
-  fs.writeFileSync(mainTsFilePath, mainTsContent);
-
-  console.log('Pinia configured successfully.');
-}
-
-function createVueShim() {
-  console.log('Creating vue-shim.d.ts file...');
-
-  // Create vue-shim.d.ts file
-  const vueShimFilePath = './src/vue-shim.d.ts';
-  const vueShimContent = `declare module '*.vue' {
-  import { ComponentOptions } from 'vue';
-  const componentOptions: ComponentOptions;
-  export default componentOptions;
-}`;
-  fs.writeFileSync(vueShimFilePath, vueShimContent);
-
-  console.log('vue-shim.d.ts file created successfully.');
+export default router;
+`
+        );
+        this.mainTsContent.imports.push("import router from './router'");
+        this.mainTsContent.uses.push("app.use(router)");
+        console.log("vue-router installed and configured successfully.");
+      },
+      pinia() {
+        console.log("Installing pinia...");
+        execSync(isUsingYarn() ? "yarn add pinia" : "npm install pinia --save");
+        console.log("Configuring pinia...");
+        this.mainTsContent.imports.push("import { createPinia } from 'pinia'");
+        this.mainTsContent.uses.push("app.use(createPinia())");
+        console.log("pinia installed and configured successfully.");
+      },
+      vueuse() {
+        console.log("Installing vueuse...");
+        execSync(isUsingYarn() ? "yarn add @vueuse/core" : "npm install @vueuse/core --save");
+        console.log("vueuse installed successfully.");
+      },
+      daisyui() {
+        console.log("Installing daisyui...");
+        execSync(isUsingYarn() ? "yarn add -D daisyui" : "npm install -D daisyui@latest");
+        console.log("Configuring daisyui...");
+        if (fs.existsSync("tailwind.config.js")) {
+          let tailwindcssJsContent = fs.readFileSync("tailwind.config.js", "utf-8");
+          tailwindcssJsContent = tailwindcssJsContent.replace(/'plugins'\s*:\s*\[\]/g, "'plugins': [require('daisyui')],");
+          fs.writeFileSync("tailwind.config.js", tailwindcssJsContent);
+        } else {
+          console.warn("tailwind.config.js not found, skipping daisyui configuration.");
+        }
+      },
+      "font-awesome"() {
+        console.log("Installing fontawesome...");
+        execSync(isUsingYarn() ? "yarn add @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/free-regular-svg-icons @fortawesome/vue-fontawesome" : "npm install @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/free-regular-svg-icons @fortawesome/vue-fontawesome --save");
+      },
+      mainTs() {
+        let mainTsContent = "";
+        for (const key in this.mainTsContent) {
+          mainTsContent += this.mainTsContent[key].join("\n") + "\n";
+        }
+        fs.writeFileSync(this.mainTsFilePath, mainTsContent);
+      },
+    };
+  }
+  installAll() {
+    let packagesToInstall = ["create", "chdir", ...this.packagesToInstall, "mainTs"];
+    for (const p of packagesToInstall) {
+      if (shouldInstall) {
+        this.cmds[p]();
+      }
+    }
+  }
 }
 
 async function main() {
@@ -144,40 +120,30 @@ async function main() {
   if (process.argv.length >= 3) {
     projectName = process.argv[2];
   } else {
-    const prompt = 'Enter a name for your project: ';
-    projectName = require('readline-sync').question(prompt);
+    const prompt = "Enter a name for your project: ";
+    projectName = require("readline-sync").question(prompt);
   }
 
-  const packagesToInstall = ['tailwindcss', 'vue-router', 'pinia'];
+  const packagesToInstall = ["tailwindcss", "vue-router", "pinia", "vueuse", "daisyui", "font-awesome"];
   const packageChoices = {};
 
-  for (const package of packagesToInstall) {
-    const prompt = `Do you want to install ${package}? (y/N): `;
-    const input = require('readline-sync').question(prompt).toLowerCase();
+  for (const pack of packagesToInstall) {
+    if (pack === "daisyui" && !packageChoices["tailwindcss"]) {
+      continue;
+    }
+    const prompt = `Do you want to install ${pack}? (y/N): `;
+    const input = require("readline-sync").question(prompt).toLowerCase();
 
-    packageChoices[package] = input === 'y';
+    packageChoices[pack] = input === "y";
   }
 
   createProject(projectName);
-  createVueShim();
-
-  for (const [package, shouldInstall] of Object.entries(packageChoices)) {
-    if (shouldInstall) {
-      installPackage(package);
-
-      if (package === 'tailwindcss') {
-        configureTailwind();
-      } else if (package === 'vue-router') {
-        configureVueRouter();
-      } else if (package === 'pinia') {
-        configurePinia();
-      }
-    }
-  }
+  const config = new MainTsConfigure({ projectName, packagesToInstall: packagesToInstall.filter((pack) => packageChoices[pack]) });
+  config.installAll();
 
   console.log(`\n🎉 Successfully created a new ${projectName} project! 🎉`);
 }
 
 main().catch((error) => {
-  console.error('An error occurred:', error);
+  console.error("An error occurred:", error);
 });
